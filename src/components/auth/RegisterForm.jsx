d64c6f70d140c0cd3d1c25391992743b7c180199
Peyton Moss <pmoss94@gmail.com>
2022-12-16 10:38:13 -0700

    register and login input catch complete

--- src/components/auth/RegisterForm.jsx
+++ src/components/auth/RegisterForm.jsx
@@ -1,15 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
 
 function RegisterFrom() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [rePassword, setRePassword] = useState("");
+
+  const inputChangeHandler = (e) => {
+    const { id, value } = e.target;
+    if (id === "username") {
+      setUsername(value);
+      console.log(value);
+    }
+    if (id === "email") {
+      setEmail(value);
+      console.log(value);
+    }
+    if (id === "password") {
+      setPassword(value);
+      console.log(value);
+    }
+    if (id === "rePassword") {
+      setRePassword(value);
+      console.log(value);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(username, email, password, rePassword);
+  };
+
   return (
     <div className="register-form">
       <h3>Create Account</h3>
-      <form>
-        <input type="text" placeholder="USERNAME" />
-        <input type="text" placeholder="EMAIL" />
-        <input type="text" placeholder="PASSWORD" />
-        <input type="text" placeholder="RE-ENTER PASSWORD" />
-        <button>JOIN</button>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input
+          type="text"
+          id="username"
+          placeholder="USERNAME"
+          value={username}
+          onChange={(e) => inputChangeHandler(e)}
+        />
+        <input
+          type="email"
+          id="email"
+          placeholder="EMAIL"
+          value={email}
+          onChange={(e) => inputChangeHandler(e)}
+        />
+        <input
+          type="text"
+          id="password"
+          placeholder="PASSWORD"
+          value={password}
+          onChange={(e) => inputChangeHandler(e)}
+        />
+        <input
+          type="text"
+          id="rePassword"
+          placeholder="RE-ENTER PASSWORD"
+          value={rePassword}
+          onChange={(e) => inputChangeHandler(e)}
+        />
+        <button type="submit">JOIN</button>
       </form>
     </div>
   );
--- src/components/auth/SignInForm.jsx
+++ src/components/auth/SignInForm.jsx
@@ -1,12 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 function SignInForm() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const inputChangeHandler = (e) => {
+    const { id, value } = e.target;
+    if (id === "username") {
+      setUsername(value);
+      console.log(value);
+    }
+    if (id === "password") {
+      setPassword(value);
+      console.log(value);
+    }
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(username, password);
+  };
+
   return (
     <div className="signIn-form">
       <h3>Sign In</h3>
-      <form>
-        <input type="text" placeholder="USERNAME" />
-        <input type="text" placeholder="PASSWORD" />
+      <form onSubmit={(e) => submitHandler(e)}>
+        <input
+          type="text"
+          id="username"
+          placeholder="USERNAME"
+          value={username}
+          onChange={(e) => inputChangeHandler(e)}
+        />
+        <input
+          type="text"
+          id="password"
+          placeholder="PASSWORD"
+          value={password}
+          onChange={(e) => inputChangeHandler(e)}
+        />
         <button>SIGN IN</button>
       </form>
     </div>
